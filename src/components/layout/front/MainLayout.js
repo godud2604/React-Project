693562ef47049e6082469f00bf0aaa2fa17471fc
godud2604 <ees238@naver.com>
2022-04-25 22:22:50 +0900

    Remove login status

--- src/components/layout/front/MainLayout.js
+++ src/components/layout/front/MainLayout.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 // components
 import { NavBottom } from './footer';
 import { HeaderNav, HeaderBack } from './header';
 // page
-import Home from '../../../pages/front/home/Home';
 
 
 const MainLayout = ({ 
@@ -23,17 +20,7 @@ const MainLayout = ({
   children, 
   loading 
 }) => {
-  const auth = useSelector((store) => store.auth);
-  const history = useHistory();
 
-  if ( auth.authStatus.isLogIn ) {
-    return (
-      <Home 
-        logInModal
-        prevPathName={history.location.pathname} 
-      />
-    )
-  }
 
   return (
     <main>
